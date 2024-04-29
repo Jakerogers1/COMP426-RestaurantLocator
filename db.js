143632df81629ef57dbef205dedddbc94dbddbed
Jake Rogers <jmanerr@ad.unc.edu>
2024-04-28 20:15:37 -0400

    removed debug database run that didn't need to be in db.js

---
+++ db.js
@@ -0,0 +1,26 @@
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./mydatabase.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+});
+
+db.run(`
+  CREATE TABLE IF NOT EXISTS restaurants (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    address TEXT NOT NULL,
+    phone TEXT,
+    image_url TEXT
+  );
+`);
+
+db.run(`CREATE TABLE IF NOT EXISTS reviews (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  restaurantId TEXT NOT NULL,
+  rating INTEGER NOT NULL,
+  text TEXT NOT NULL
+)`);
+
+module.exports = db;
